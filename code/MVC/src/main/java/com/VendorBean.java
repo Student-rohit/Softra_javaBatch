7afb56ef9aa64a378d59a8f3df50f27c97a7e4d7
rohit bandgar <153342850+Student-rohit@users.noreply.github.com>
2026-05-13 20:33:27 +0530

    Bean

---
+++ code/MVC/src/main/java/com/VendorBean.java
@@ -0,0 +1,36 @@
+package com;
+
+public class VendorBean {
+
+    private Integer venId;
+    private String venName;
+    private Integer mobile;
+
+    public VendorBean() {
+
+    }
+
+    public Integer getVenId() {
+        return venId;
+    }
+
+    public void setVenId(Integer venId) {
+        this.venId = venId;
+    }
+
+    public String getVenName() {
+        return venName;
+    }
+
+    public void setVenName(String venName) {
+        this.venName = venName;
+    }
+
+    public Integer getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(Integer mobile) {
+        this.mobile = mobile;
+    }
+}
