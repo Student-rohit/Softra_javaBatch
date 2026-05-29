e31dcbf9dd6ecd0258509a51efd2e23b6d396ad1
rohit bandgar <153342850+Student-rohit@users.noreply.github.com>
2026-05-29 22:24:11 +0530

    Config

---
+++ Spring/annotation/Config.java
@@ -0,0 +1,10 @@
+package com.core.annotation;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.core.annotation")
+public class Config {
+
+}
