31ee41a4466e5c349de1f2c494f05078924f3ba3
rohit bandgar <153342850+Student-rohit@users.noreply.github.com>
2026-06-30 23:36:37 +0530

    DB Config

---
+++ Spring Boot/myfirst/src/main/java/com/example/learn/myfirst/DataBasePropertyConfiguration.java
@@ -0,0 +1,37 @@
+package com.example.learn.myfirst;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "db-service")
+public class DataBasePropertyConfiguration {
+
+    private String url;
+    private String user;
+    private String pwd;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
